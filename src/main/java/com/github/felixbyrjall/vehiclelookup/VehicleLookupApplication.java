0765cc82568f5df5f6e3144f161db80a7b4ffc9c
Felix Byrjall <felixbyrjall@gmail.com>
2024-09-05 16:46:49 +0200

    chore: initial commit set up with spring boot

---
+++ src/main/java/com/github/felixbyrjall/vehiclelookup/VehicleLookupApplication.java
@@ -0,0 +1,13 @@
+package com.github.felixbyrjall.vehiclelookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleLookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/felixbyrjall/vehiclelookup/VehicleLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.felixbyrjall.vehiclelookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
