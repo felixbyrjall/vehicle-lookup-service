4a3cc1ced73ffd0cf9c6baaaff2740dcf103dc02
Felix Byrjall <felixbyrjall@gmail.com>
2024-10-07 21:20:00 +0200

    fix: bug in AdditionalDataService when logging out the DTO object fixed a bug where the log would output "Enriched vehicle data: com.github.felixbyrjall.vehiclelookup.dto.AdditionalDataDTO@ed7356d" instead of the correct "Enriched vehicle data: AdditionalDataDTO(county=Vestland,  geographicalArea=Bergen, vehicleType=null)". Also removed licensePlatePrefix field, as it is unused

--- src/main/java/com/github/felixbyrjall/vehiclelookup/dto/AdditionalDataDTO.java
+++ src/main/java/com/github/felixbyrjall/vehiclelookup/dto/AdditionalDataDTO.java
@@ -2,11 +2,12 @@ package com.github.felixbyrjall.vehiclelookup.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class AdditionalDataDTO {
-    private String licensePlatePrefix;
     private String county; // Fylke
     private String geographicalArea;
     private String vehicleType; // For special combinations like "Elektriskdrevet kjøretøy"
